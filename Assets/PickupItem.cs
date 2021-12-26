ea1ff958dbaab4af97c6772316802a957f4e0761
GriggsGD <79928221+GriggsGD@users.noreply.github.com>
2021-12-26 13:18:48 +0000

    Add files via upload

---
+++ Assets/PickupItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItem : MonoBehaviour
+{
+    public void Pickup()
+    {
+        Destroy(gameObject);
+    }
+}
